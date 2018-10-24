75502d936ab4170026143501b595b72432ac2927
MiriamLeis <mirileis@ucm.es>
2018-10-24 19:06:15 +0200

    Cambiado tamaño de pantalla

--- FNAF/src/js/main.js
+++ FNAF/src/js/main.js
@@ -57,7 +57,7 @@ var PreloaderScene = {
 
 
 window.onload = function () {
-  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
+  var game = new Phaser.Game(792, 594, Phaser.AUTO, 'game');
 
   game.state.add('boot', BootScene);
   game.state.add('preloader', PreloaderScene);
