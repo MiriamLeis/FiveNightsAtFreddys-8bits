bf67bbd0c6ef1d2e4c644a1918802dac6047e4f3
MiriamLeis <mirileis@ucm.es>
2018-11-04 13:05:45 +0100

    Añadidos espacios

--- FNAF/src/js/cameraScene.js
+++ FNAF/src/js/cameraScene.js
@@ -14,7 +14,7 @@ var CameraScene =
         var tamX = this.game.world.width;
         var tamY = this.game.world.height;
 
-        this.game.world.resize(tamX * 11, tamY * 11);
+        this.game.world.resize(tamX * 10, tamY * 10);
 
     //Cameras
         var Rooms =
@@ -48,34 +48,31 @@ var CameraScene =
         }
 
     //Buttons
-        this.cam1A = addButton(this.game,Rooms,Rooms.ShowStage, 0, 0, 0.05);
-        this.cam1B = addButton(this.game,Rooms,Rooms.DinningRoom, -1, 0, 0.05);
-        this.cam5 = addButton(this.game,Rooms,Rooms.Backstage, -2, 0, 0.05);
-        this.cam7 = addButton(this.game,Rooms,Rooms.Restrooms, -3, 0, 0.05);
-        this.cam6 = addButton(this.game,Rooms,Rooms.Kitchen, -4, 0, 0.05);
-        this.cam4A = addButton(this.game,Rooms,Rooms.EastHall, -5, 0, 0.05);
-        this.cam3 = addButton(this.game,Rooms,Rooms.SupplyCloset, -6, 0, 0.05);
-        this.cam4B = addButton(this.game,Rooms,Rooms.EHallCorner, -7, 0, 0.05);
-        this.cam2A = addButton(this.game,Rooms,Rooms.WestHall, -8, 0, 0.05);
-        this.cam2B = addButton(this.game,Rooms,Rooms.WHallCorner, -9, 0, 0.05);
-        this.cam1C = addButton(this.game,Rooms,Rooms.PirateCove, -10, 0, 0.05);
+        this.cam1A = addButton(this.game,Rooms,Rooms.ShowStage, 0, 0, 0.05, 0.05);
+        this.cam1B = addButton(this.game,Rooms,Rooms.DinningRoom, -1, 0, 0.05, 0.05);
+        this.cam5 = addButton(this.game,Rooms,Rooms.Backstage, -2, 0, 0.05, 0.05);
+        this.cam7 = addButton(this.game,Rooms,Rooms.Restrooms, -3, 0, 0.05, 0.05);
+        this.cam6 = addButton(this.game,Rooms,Rooms.Kitchen, -4, 0, 0.05, 0.05);
+        this.cam4A = addButton(this.game,Rooms,Rooms.EastHall, -5, 0, 0.05, 0.05);
+        this.cam3 = addButton(this.game,Rooms,Rooms.SupplyCloset, -6, 0, 0.05, 0.05);
+        this.cam4B = addButton(this.game,Rooms,Rooms.EHallCorner, -7, 0, 0.05, 0.05);
+        this.cam2A = addButton(this.game,Rooms,Rooms.WestHall, -8, 0, 0.05, 0.05);
+        this.cam2B = addButton(this.game,Rooms,Rooms.WHallCorner, -9, 0, 0.05, 0.05);
+        this.cam1C = addButton(this.game,Rooms,Rooms.PirateCove, -10, 0, 0.05, 0.05);
 
 
     //Draw cameras
-        
-        addCamera( Rooms.cameraPositions[Rooms.ShowStage],tamX,tamY,1.5);
-
-        addCamera( Rooms.cameraPositions[Rooms.DinningRoom],tamX,tamY,1.5);
-        addCamera( Rooms.cameraPositions[Rooms.Backstage],tamX,tamY,1.5);
-        addCamera( Rooms.cameraPositions[Rooms.Restrooms],tamX,tamY,1.5);
-        addCamera( Rooms.cameraPositions[Rooms.Kitchen],tamX,tamY,1.5);
-
-        addCamera( Rooms.cameraPositions[Rooms.EastHall],tamX,tamY,1.5);
-        addCamera( Rooms.cameraPositions[Rooms.SupplyCloset],tamX,tamY,1.5);
-        addCamera( Rooms.cameraPositions[Rooms.EHallCorner],tamX,tamY,1.5);
-        addCamera( Rooms.cameraPositions[Rooms.WestHall],tamX,tamY,1.5);
-        addCamera( Rooms.cameraPositions[Rooms.WHallCorner],tamX,tamY,1.5);
-        addCamera( Rooms.cameraPositions[Rooms.PirateCove],tamX,tamY,1.5);
+        addCamera( Rooms.cameraPositions[Rooms.ShowStage], tamX, tamY, 1.5);
+        addCamera( Rooms.cameraPositions[Rooms.DinningRoom], tamX, tamY, 1.5);
+        addCamera( Rooms.cameraPositions[Rooms.Backstage], tamX, tamY, 1.5);
+        addCamera( Rooms.cameraPositions[Rooms.Restrooms], tamX, tamY, 1.5);
+        addCamera( Rooms.cameraPositions[Rooms.Kitchen], tamX, tamY, 1.5);
+        addCamera( Rooms.cameraPositions[Rooms.EastHall], tamX, tamY, 1.5);
+        addCamera( Rooms.cameraPositions[Rooms.SupplyCloset], tamX, tamY, 1.5);
+        addCamera( Rooms.cameraPositions[Rooms.EHallCorner], tamX, tamY, 1.5);
+        addCamera( Rooms.cameraPositions[Rooms.WestHall], tamX, tamY, 1.5);
+        addCamera( Rooms.cameraPositions[Rooms.WHallCorner], tamX, tamY, 1.5);
+        addCamera( Rooms.cameraPositions[Rooms.PirateCove], tamX, tamY, 1.5);
 
     },
 
@@ -88,29 +85,25 @@ function actionOnClick(game, Rooms, n)
 {
     game.camera.x = Rooms.cameraPositions[n].x;
     game.camera.y = Rooms.cameraPositions[n].y;
-
+    console.log ('hey');
 }
 
 
-function addButton(game, Rooms, n,posX,posY,tam)
+function addButton(game, Rooms, n, posX, posY, tamX, tamY)
 {
-   var buttom = game.add.button(0, 0, 'button', function (){actionOnClick(game, Rooms, n)}, this);
-    buttom.scale.setTo(tam,tam );
+    var buttom = game.add.button(0, 0, 'button', function (){actionOnClick(game, Rooms, n)}, this);
+    buttom.scale.setTo(tamX, tamY);
     buttom.anchor.setTo(posX, posY);
     buttom.fixedToCamera = true;
 
     return buttom;
-
 }
 
-function addCamera(camera, tamX, tamY,tam)
+function addCamera(camera, tamX, tamY, tam)
 {
     camera.sprite.scale.setTo(tam, tam);
-
     camera.sprite.x =  camera.x + (tamX - camera.sprite.width)/2;
-        
-    camera.sprite.y =  camera.y + (tamY - camera.sprite.height)/2;
-    
+    camera.sprite.y =  camera.y + (tamY - camera.sprite.height)/2; 
 }
 
 
