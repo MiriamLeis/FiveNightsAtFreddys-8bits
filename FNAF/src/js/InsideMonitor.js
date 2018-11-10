6489c0c1b7f937afa39eabc2f9f29972a0057ff5
pablojor <pablojor@ucm.es>
2018-11-10 15:45:15 +0100

    Pequeño cambio

--- FNAF/src/js/InsideMonitor.js
+++ FNAF/src/js/InsideMonitor.js
@@ -2,16 +2,16 @@ function InsideMonitor (game, Rooms)
 {
     this.cam1A = this.addButton(game, Rooms, Rooms.ShowStage, 0, 0,this);
     this.cam1A.frame = 1;
-    this.cam1B = this.addButton(game, Rooms, Rooms.DinningRoom, -1, 0,this /*0.05, 0.05*/);
-    this.cam5 = this.addButton(game, Rooms, Rooms.Backstage, -2, 0,this /*0.05, 0.05*/);
-    this.cam7 = this.addButton(game, Rooms, Rooms.Restrooms, -3, 0,this /*0.05, 0.05*/);
-    this.cam6 = this.addButton(game, Rooms,  Rooms.Kitchen, -4, 0,this /*0.05, 0.05*/);
-    this.cam4A = this.addButton(game, Rooms, Rooms.EastHall, -5, 0,this /*0.05, 0.05*/);
-    this.cam3 = this.addButton(game, Rooms, Rooms.SupplyCloset, -6, 0,this /*0.05, 0.05*/);
-    this.cam4B = this.addButton(game, Rooms, Rooms.EHallCorner, -7, 0,this /*0.05, 0.05*/);
-    this.cam2A = this.addButton(game, Rooms, Rooms.WestHall, -8, 0,this /*0.05, 0.05*/);
-    this.cam2B = this.addButton(game, Rooms, Rooms.WHallCorner, -9, 0,this /*0.05, 0.05*/);
-    this.cam1C = this.addButton(game, Rooms, Rooms.PirateCove, -10, 0,this /*0.05, 0.05*/);
+    this.cam1B = this.addButton(game, Rooms, Rooms.DinningRoom, -1, 0,this);
+    this.cam5 = this.addButton(game, Rooms, Rooms.Backstage, -2, 0,this);
+    this.cam7 = this.addButton(game, Rooms, Rooms.Restrooms, -3, 0,this);
+    this.cam6 = this.addButton(game, Rooms,  Rooms.Kitchen, -4, 0,this);
+    this.cam4A = this.addButton(game, Rooms, Rooms.EastHall, -5, 0,this);
+    this.cam3 = this.addButton(game, Rooms, Rooms.SupplyCloset, -6, 0,this);
+    this.cam4B = this.addButton(game, Rooms, Rooms.EHallCorner, -7, 0,this);
+    this.cam2A = this.addButton(game, Rooms, Rooms.WestHall, -8, 0,this);
+    this.cam2B = this.addButton(game, Rooms, Rooms.WHallCorner, -9, 0,this);
+    this.cam1C = this.addButton(game, Rooms, Rooms.PirateCove, -10, 0,this);
 
 
 };
@@ -38,7 +38,6 @@ InsideMonitor.prototype.addButton = function(game, Rooms, n, posX, posY, button)
 {
     var button = game.add.button(0, 0, 'buttonCameras', function (){this.changeActive(button, game, Rooms, n)}, this);
     
-    //buttom.scale.setTo(tamX, tamY);
     button.anchor.setTo(posX, posY);
     button.fixedToCamera = true;
 
